e07175c19e2eb48cc5bf5e0113d7830f75d6aa1a
r04944035 <r04944035@linux5.csie.ntu.edu.tw>
2016-03-01 17:19:49 +0800

    lab0

---
+++ lab0/main.cu
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <cstdlib>
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "SyncedMemory.h"
+
+#define CHECK {\
+	auto e = cudaDeviceSynchronize();\
+	if (e != cudaSuccess) {\
+		printf("At " __FILE__ ":%d, %s\n", __LINE__, cudaGetErrorString(e));\
+		abort();\
+	}\
+}
+
+__global__ void SomeTransform(char *input_gpu, int fsize) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < fsize && input_gpu[idx] >= 'a') {
+		input_gpu[idx] += ('A'-'a');
+	}
+}
+
+int main(int argc, char **argv)
+{
+	// init, and check
+	if (argc != 2) {
+		printf("Usage %s <input text file>\n", argv[0]);
+		abort();
+	}
+	FILE *fp = fopen(argv[1], "r");
+	if (!fp) {
+		printf("Cannot open %s", argv[1]);
+		abort();
+	}
+	// get file size
+	fseek(fp, 0, SEEK_END);
+	size_t fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	
+	int blocksize = 8;
+	int nblock = fsize/blocksize + (fsize % blocksize == 0 ? 0 : 1);
+	// read files
+	MemoryBuffer<char> text(fsize+1);
+	auto text_smem = text.CreateSync(fsize);
+	CHECK;
+	fread(text_smem.get_cpu_wo(), 1, fsize, fp);
+	text_smem.get_cpu_wo()[fsize] = '\0';
+	fclose(fp);
+
+	// TODO: do your transform here
+	char *input_gpu = text_smem.get_gpu_rw();
+	// An example: transform the first 64 characters to '!'
+	// Don't transform over the tail
+	// And don't transform the line breaks
+	
+	SomeTransform<<<nblock, blocksize>>>(input_gpu, fsize);
+
+	puts(text_smem.get_cpu_ro());
+	return 0;
+}
