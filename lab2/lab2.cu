a38c20cc077d7e057c5fe41152c47a842d53221e
r04944035 <r04944035@linux5.csie.ntu.edu.tw>
2016-04-06 00:38:27 +0800

    lab2

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -1,6 +1,7 @@
 #include "lab2.h"
 #include <cmath>
 #include <algorithm>
+
 static const unsigned W = 640;
 static const unsigned H = 480;
 static const unsigned NFRAME = 240;
@@ -23,77 +24,79 @@ int p[] = {151,160,137,91,90,15,
  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
- // To remove the need for index wrapping, double the permutation table length
+// To remove the need for index wrapping, double the permutation table length
  
 int *perm = new int[512];
- //for(int i=0; i<512; i++) perm[i]=p[i & 255];
 
- 
- int fastfloor(double x) {
- return x>0 ? (int)x : (int)x-1;
-}
-
-double dot(int g[], double x, double y) {
-  return g[0]*x + g[1]*y; 
-}
-
-// 2D simplex noise
-double noise(double xin, double yin) {
-	 double n0, n1, n2; // Noise contributions from the three corners
-	 // Skew the input space to determine which simplex cell we're in
-	 double F2 = 0.5*(sqrt(3.0)-1.0);
-	 double s = (xin+yin)*F2; // Hairy factor for 2D
-	 int i = fastfloor(xin+s);
-	 int j = fastfloor(yin+s);
-	 double G2 = (3.0-sqrt(3.0))/6.0;
-	 double t = (i+j)*G2;
-	 double X0 = i-t; // Unskew the cell origin back to (x,y) space
-	 double Y0 = j-t;
-	 double x0 = xin-X0; // The x,y distances from the cell origin
-	 double y0 = yin-Y0;
-	 // For the 2D case, the simplex shape is an equilateral triangle.
-	 // Determine which simplex we are in.
-	 int i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords
-	 if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1)
-	 else {i1=0; j1=1;} // upper triangle, YX order: (0,0)->(0,1)->(1,1)
-	 // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
-	 // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
-	 // c = (3-sqrt(3))/6
-	 double x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
-	 double y1 = y0 - j1 + G2;
-	 double x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords
-	 double y2 = y0 - 1.0 + 2.0 * G2;
-	 // Work out the hashed gradient indices of the three simplex corners
-	 int ii = i & 255;
-	 int jj = j & 255;
-	 int gi0 = perm[ii+perm[jj]] % 12;
-	 int gi1 = perm[ii+i1+perm[jj+j1]] % 12;
-	 int gi2 = perm[ii+1+perm[jj+1]] % 12;
-	 // Calculate the contribution from the three corners
-	 double t0 = 0.5 - x0*x0-y0*y0;
-	 if(t0<0) n0 = 0.0;
-	 else {
-	 t0 *= t0;
-	 n0 = t0 * t0 * dot(grad3[gi0], x0, y0); // (x,y) of grad3 used for 2D gradient
-	 }
-	 double t1 = 0.5 - x1*x1-y1*y1;
-	 if(t1<0) n1 = 0.0;
-	 else {
-	 t1 *= t1;
-	 n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
-	 }
-	 double t2 = 0.5 - x2*x2-y2*y2;
-	 if(t2<0) n2 = 0.0;
-	 else {
-	 t2 *= t2;
-	 n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
-	 }
-	 // Add contributions from each corner to get the final noise value.
-	 // The result is scaled to return values in the interval [-1,1].
-	 return 70.0 * (n0 + n1 + n2);
- }
 struct Lab2VideoGenerator::Impl {
 	int t = 0;
+	int fastfloor(double x) {
+		return x>0 ? (int)x : (int)x-1;
+	}
+
+	double dot(int g[], double x, double y) {
+		return g[0]*x + g[1]*y; 
+	}
+	
+	void initialize() {
+		for(int i=0; i<512; i++) perm[i]=p[i & 255];
+	}
+	
+	// 2D simplex noise
+	double noise(double xin, double yin) {
+		double n0, n1, n2; // Noise contributions from the three corners
+		// Skew the input space to determine which simplex cell we're in
+		double F2 = 0.5*(sqrt(3.0)-1.0);
+		double s = (xin+yin)*F2; // Hairy factor for 2D
+		int i = fastfloor(xin+s);
+		int j = fastfloor(yin+s);
+		double G2 = (3.0-sqrt(3.0))/6.0;
+		double t = (i+j)*G2;
+		double X0 = i-t; // Unskew the cell origin back to (x,y) space
+		double Y0 = j-t;
+		double x0 = xin-X0; // The x,y distances from the cell origin
+		double y0 = yin-Y0;
+		// For the 2D case, the simplex shape is an equilateral triangle.
+		// Determine which simplex we are in.
+		int i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords
+		if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1)
+		else {i1=0; j1=1;} // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+		// A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
+		// a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
+		// c = (3-sqrt(3))/6
+		double x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
+		double y1 = y0 - j1 + G2;
+		double x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords
+		double y2 = y0 - 1.0 + 2.0 * G2;
+		// Work out the hashed gradient indices of the three simplex corners
+		int ii = i & 255;
+		int jj = j & 255;
+		int gi0 = perm[ii+perm[jj]] % 12;
+		int gi1 = perm[ii+i1+perm[jj+j1]] % 12;
+		int gi2 = perm[ii+1+perm[jj+1]] % 12;
+		// Calculate the contribution from the three corners
+		double t0 = 0.5 - x0*x0-y0*y0;
+		if(t0<0) n0 = 0.0;
+		else {
+		t0 *= t0;
+		n0 = t0 * t0 * dot(grad3[gi0], x0, y0); // (x,y) of grad3 used for 2D gradient
+		}
+		double t1 = 0.5 - x1*x1-y1*y1;
+		if(t1<0) n1 = 0.0;
+		else {
+		t1 *= t1;
+		n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
+		}
+		double t2 = 0.5 - x2*x2-y2*y2;
+		if(t2<0) n2 = 0.0;
+		else {
+		t2 *= t2;
+		n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
+		}
+		// Add contributions from each corner to get the final noise value.
+		// The result is scaled to return values in the interval [-1,1].
+		return 70.0 * (n0 + n1 + n2);
+	}
 };
 
 Lab2VideoGenerator::Lab2VideoGenerator(): impl(new Impl) {
@@ -111,12 +114,15 @@ void Lab2VideoGenerator::get_info(Lab2VideoInfo &info) {
 	info.fps_d = 1;
 };
 
+
 void Lab2VideoGenerator::Generate(uint8_t *yuv) {
+	impl->initialize();
 	cudaMemset(yuv, 0, W*H);
-	std::random_shuffle(p, p+256);
-	for(int i=0; i<512; i++) perm[i]=p[i & 255];
-	for(int i = 0; i < W*H/2; ++i) {
-		cudaMemset(yuv+W*H+i, perm[i], 1);
+	//cudaMemset(yuv+W*H, 128, W*H/2);
+	(impl->t)++;
+	int len = W*H;
+	std::random_shuffle(perm, perm+512);
+	for(int i = 0; i <= W/2; ++i) {
+		cudaMemset(yuv+W*H+i, impl->noise(i/W,i%W), 1);
 	}
-	++(impl->t);
 }
